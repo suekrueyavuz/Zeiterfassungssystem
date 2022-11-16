fad5dd09307e5b7fc4eadaf565ce2e6b6d7b1efa
Sükrü Yavuz <yavuz.suekrue@gmail.com>
2022-11-16 20:31:17 +0100

    scheduler entfernt

--- src/main/java/fhcampuswien/zeiterfassungssystem/ZeiterfassungssystemApplication.java
+++ src/main/java/fhcampuswien/zeiterfassungssystem/ZeiterfassungssystemApplication.java
@@ -11,11 +11,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
-@EnableScheduling
 public class ZeiterfassungssystemApplication implements CommandLineRunner {
 	@Autowired private MitarbeiterService mitarbeiterService;
 	@Autowired private AuftraggeberFirmaService auftraggeberFirmaService;
